4bd36ee106631775f66e06f4caa823f52b9cc44d
daniel-dios <daniel.dios@n26.com>
2023-05-02 20:35:28 +0200

    example working with udp same accumulator and tcp multi user

--- golden/src/main/java/NIOClient.java
+++ golden/src/main/java/NIOClient.java
@@ -10,35 +10,29 @@ public class NIOClient {
     private static final int PORT = 8080;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        InetSocketAddress serverAddress = new InetSocketAddress(HOST, PORT);
+        final var serverAddress = new InetSocketAddress(HOST, PORT);
 
-        // Crear el cliente y establecer conexión con el servidor
-        SocketChannel client = SocketChannel.open(serverAddress);
+        final var client = SocketChannel.open(serverAddress);
 
-        ByteBuffer buffer = ByteBuffer.allocate(10);
-        final Scanner scanner = new Scanner(System.in);
+        final var buffer = ByteBuffer.allocate(10);
+        final var scanner = new Scanner(System.in);
 
         for (int i = 0; i < 10; i++) {
-            final String s = scanner.nextLine();
-            System.out.println("introduced: " + s);
-            // Enviar el dígito al servidor
+            scanner.nextLine();
             buffer.put((byte) i);
             buffer.flip();
             client.write(buffer);
 
-            // Leer la respuesta del servidor
             buffer.clear();
             client.read(buffer);
             buffer.flip();
             int response = buffer.get();
 
-            // Imprimir la respuesta del servidor
             System.out.println("Respuesta del servidor para el dígito " + (i) + ": " + response);
 
             buffer.clear();
         }
 
-        // Cerrar la conexión
         client.close();
     }
 }
